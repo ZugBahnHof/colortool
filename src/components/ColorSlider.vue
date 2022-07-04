074505cbd30a2bd530bf6d5d4f0b9fbaaed89642
Julian Leucker <leuckerj@gmail.com>
2022-07-04 20:41:48 +0200

    Create Slider Component

---
+++ src/components/ColorSlider.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: {'modelValue': [String, Number], "isAlpha": Boolean},
+  emits: ['update:modelValue']
+}
+</script>
+
+<template>
+  <input v-if="!isAlpha" type="range" min="0" max="255"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+  />
+
+  <input v-else type="range" min="0" max="1" step="0.00390625"
+         :value="modelValue"
+         @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
